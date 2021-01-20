7138b7351d4315f948399a4bf1243ede9586b693
kyleyiyang <kyleyiyang@gmail.com>
2021-01-19 22:32:11 -0500

    Update minDepth.java

--- minDepth.java
+++ minDepth.java
@@ -19,3 +19,25 @@ class Solution {
         return (root.left!=null && root.right!=null) ? (Math.min(minDepth(root.left),minDepth(root.right))+1) :(Math.max(minDepth(root.left),minDepth(root.right))+1);
     }
 }
+// BFS
+public int minDepth(TreeNode root) {
+        if(root == null)
+            return 0;
+        int depth = 1;
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i = 0; i<size; i++){
+                TreeNode curr = q.poll();
+                if(curr != null){
+                    if(curr.left == null && curr.right == null)
+                        return depth;
+                    q.add(curr.left);
+                    q.add(curr.right);
+                }
+            }
+            depth++;
+         }
+        return depth;
+    }
